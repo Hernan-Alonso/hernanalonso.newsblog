55aeb2860388db5910ba2cab542078af44167733
Operador <Operador@NB-002630.hlgv.net>
2019-04-22 17:21:43 -0300

    Primer commit

---
+++ precache-manifest.f709db85a7be1c8a26300c4b30e4904e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a18753cc1fe742d99852",
+    "url": "/static/js/main.2222f6b9.chunk.js"
+  },
+  {
+    "revision": "88a74bde9538760063ee",
+    "url": "/static/js/2.5b5f65c7.chunk.js"
+  },
+  {
+    "revision": "a18753cc1fe742d99852",
+    "url": "/static/css/main.2e761e4b.chunk.css"
+  },
+  {
+    "revision": "d9c1c767b5870b0cbfe9fa8b1d63907d",
+    "url": "/index.html"
+  }
+];
